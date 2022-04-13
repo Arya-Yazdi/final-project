29718471894698655d17c4f8b01805811cb42b24
Arya <aryaatarzadeh@gmail.com>
2022-04-13 19:07:49 +0800

    completed 02

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-import os
-
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from profanity_filter import ProfanityFilter
-from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from login_required import login_required
